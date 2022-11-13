66409a87b6c7f36b5d412e47cf3f4f28ddf4d1ed
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-11-13 22:15:55 +0530

    commit :: inference

---
+++ 16.Inference/script.ts
@@ -0,0 +1,23 @@
+/*Type inference means getting type of variable(i.e =number , string , any ) on the basis of there value */
+/* type : number */
+let data =20;
+
+/* type : boolean */
+let boolean = true;
+
+/* type : string */
+let string = "string";
+
+/* If variable value is not assigned with value then there type will be any */
+let user;
+
+user = 10;
+user = true;
+user = "string";
+
+
+/*set constant  type of data ::: type : 5 */
+let constant :5; 
+
+
+
