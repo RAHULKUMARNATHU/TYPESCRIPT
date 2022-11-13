572df4b639adc8bdd25357c4cc4dd5c9bac347a4
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-11-13 23:57:00 +0530

    undefined

---
+++ FunctionType/script.js
@@ -0,0 +1,7 @@
+/*function type =  what is the return type of a function*/
+var a = 10;
+var b = 20;
+function sum() {
+    return a + b;
+}
+console.log(a + b);
---
+++ FunctionType/script.ts
@@ -0,0 +1,16 @@
+/*function type =  what is the return type of a function*/
+
+const a =10;
+const b= 20
+function sum () :number
+{
+    return a+b ;
+}
+
+console.log(a+b);
+
+
+function und():undefined
+{
+return;
+}
