07bbea0acff7a7fb73cc93e8c2d49c387ec52cfd
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-11-13 23:05:51 +0530

    commit ::tuple

---
+++ 17.tuple/tuple.js
@@ -0,0 +1,8 @@
+// Array
+var role = ['admin', 'user'];
+[10, 20, 1, 30];
+/* Array to tuple */
+var tuple = [10, "tuple"];
+tuple.push(true);
+tuple[1] = "Nathu";
+console.log(tuple);
---
+++ 17.tuple/tuple.ts
@@ -0,0 +1,11 @@
+
+// Array
+
+let role : string[] | number[] = ['admin','user' ];[10 ,20,1,30];
+
+/* Array to tuple */
+
+const tuple : [number , string , boolean?] = [10 , "tuple"]; 
+tuple.push(true);
+tuple[1] = "Nathu";
+console.log(tuple)
