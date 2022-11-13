4da4922895f5a169b4e58af2ddb62ece6eedaf3a
RAHUL KUMAR <rahulkumars9572@gmail.com>
2022-11-13 09:33:10 +0530

    commit :: done

---
+++ 15.tsconfig/index.ts
@@ -0,0 +1,5 @@
+/* to create tsconfig file -----> tsc --init */
+
+/*get Output file (i.e js file)  to a  director ( i.e public) ---> tsconfig => rootDir : "./src"  , outDir : "./public" */
+
+/*If ts file at root dir ----> tsconfig  "include" : ["src"]*/
