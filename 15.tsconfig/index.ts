5ec3f2c1264072ed0996f3eeacbd2ff5847f1abc
Rahul Kumar <rahul.kumar@brainvire.com>
2022-11-14 11:09:39 +0530

    commit :: watch mode

--- 15.tsconfig/index.ts
+++ 15.tsconfig/index.ts
@@ -3,3 +3,5 @@
 /*get Output file (i.e js file)  to a  director ( i.e public) ---> tsconfig => rootDir : "./src"  , outDir : "./public" */
 
 /*If ts file at root dir ----> tsconfig  "include" : ["src"]*/
+
+/*tsc -w  || tsc --watch */
