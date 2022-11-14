839681d03a1023b083a5ca3a989162a7b701df15
Rahul Kumar <rahul.kumar@brainvire.com>
2022-11-14 17:53:00 +0530

    commit :: done type alias

--- 19.Type Alias/alias.tsx
+++ 19.Type Alias/alias.tsx
@@ -1,9 +1,19 @@
-
 type aliasType = string | number | undefined;
 
+let a: aliasType = "nathu";
+let b: aliasType = 25;
+let c: aliasType = undefined;
 
-let a  : aliasType ="nathu";
-let b : aliasType = 25;
-let c  : aliasType =undefined;
+/* create Alias type i.e User */
 
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
+};
 
+function createUser(User: User): User {
+  return { name: "nathu", email: "nathi@gmail.com", isActive: false };
+}
+
+createUser({ name: "nathu", email: "nathi@gmail.com", isActive: false });
