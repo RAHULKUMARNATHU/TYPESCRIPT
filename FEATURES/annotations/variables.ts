c529918961f71748ca9d804341a173414f084d01
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-21 19:18:25 +0530

    commit :: functions

--- FEATURES/annotations/variables.ts
+++ FEATURES/annotations/variables.ts
@@ -42,7 +42,7 @@ console.log(coordinates); // {x:10 , y:20};
 //and initialization it later
 
 let words = ["red", "green", "blue"];
-let foundWord :boolean;
+let foundWord: boolean;
 
 for (let i = 0; i < words.length; i++) {
   if (words[i] === "green") {
@@ -50,6 +50,12 @@ for (let i = 0; i < words.length; i++) {
   }
 }
 
-
-
 // 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero:boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
