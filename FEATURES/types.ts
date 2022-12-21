4437f0e005cd16a2ae86e76149dcda0057d55aa4
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-21 11:08:50 +0530

    commit :: first commit

---
+++ FEATURES/types.ts
@@ -0,0 +1,22 @@
+const today = new Date();
+today.getMonth();
+
+
+const person ={
+    age:24
+}
+
+person.age=20;
+console.log(person.age);
+
+
+
+class Color{
+
+}
+
+
+
+const red = new Color();
+
+console.log(red);
