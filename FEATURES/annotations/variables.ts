d6373bb5e0e08a2e566b3203cdbacce43d04e7b5
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-21 11:16:29 +0530

    commit: variables

--- FEATURES/annotations/variables.ts
+++ FEATURES/annotations/variables.ts
@@ -7,3 +7,22 @@ let nothing: undefined = undefined;
 
 // built in Objects
 let now: Date = new Date();
+
+// Array
+let colors: string[] = ["red", "blue", "green"];
+
+let myNumbers: number[] = [1, 2, 3];
+let truths: boolean[] = [true, false, true];
+
+// Classes
+class Car {}
+
+let car: Car = new Car();
+
+// Object literals
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
