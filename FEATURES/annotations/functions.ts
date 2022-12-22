42e2a2ea783f1c93c45bad1f66e41251282836a6
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-22 12:42:48 +0530

    commit :: functions

--- FEATURES/annotations/functions.ts
+++ FEATURES/annotations/functions.ts
@@ -1,3 +1,52 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a + b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+  // return undefined;
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(todaysWeather);
+
+const logWeather2 = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather2(todaysWeather);
+
+// ES2015
+const logtodaysWeather = ({ date, weather }) => {
+  console.log(date);
+  console.log(weather);
+};
+logtodaysWeather(todaysWeather);
