0fe29b4e324d35688b1bdb370cf44e1ab2e34811
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-22 17:22:13 +0530

    commit :: tuples

---
+++ FEATURES/tuples.ts
@@ -0,0 +1,19 @@
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+};
+type Drinks = [string, boolean, number];
+
+const pepsi: Drinks = ["brown", true, 40];
+const sprite: Drinks = ["clear", false, 50];
+const coca: Drinks = ["brown", true, 40];
+
+//
+const carSpecs: [number, number] = [400, 3354];
+
+
+const carStats = {
+  horsepower: 400,
+  weight: 3354,
+};
