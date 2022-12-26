bac994fb52dedbf8c1e28ce9d6f5cd65e2fc80de
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-26 11:00:09 +0530

    commit:: class method modifiers

--- FEATURES/classes.ts
+++ FEATURES/classes.ts
@@ -1,18 +1,22 @@
 class Vehicle {
-  drive(): void {
-    console.log("chugga chugga");
-  }
-  honk(): void {
+  //   public drive(): void {
+  //     console.log("chugga chugga");
+  //   }
+  protected honk(): void {
     console.log("beep");
   }
 }
-
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log("vroom");
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car();
-car.drive();
-car.honk();
+car.startDrivingProcess();
+car.startDrivingProcess();
